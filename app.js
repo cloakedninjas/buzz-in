80b9040893314d89c451208fb7c8c7665ef722d1
Daniel Jackson <daniel@cloakedninjas.co.uk>
2019-02-03 22:18:15 +0000

    SCSS > SASS, save folder

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
+const fs = require("fs");
+const path = require('path');
+
 const createError = require('http-errors');
 const express = require('express');
-const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const sassMiddleware = require('node-sass-middleware');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const hostRouter = require('./routes/host');
+
+
+if (!fs.exists('data/save.json')) {
+  fs.writeFileSync('data/save.json', '{}');
+}
 
 const app = express();
 
@@ -21,13 +28,13 @@ app.use(cookieParser());
 app.use(sassMiddleware({
   src: path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
-  indentedSyntax: true, // true = .sass and false = .scss
+  indentedSyntax: false, // true = .sass and false = .scss
   sourceMap: true
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/host', hostRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -35,7 +42,7 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function(err, req, res) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
